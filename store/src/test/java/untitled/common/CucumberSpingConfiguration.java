ecdeab65b71292ce347ed12d8c872bae6dd3741f
jinseonyoon <wlstjs9492@gmail.com>
2022-07-26 01:32:16 +0000

    2020726초기화

--- store/src/test/java/untitled/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package untitled.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import untitled.StoreApplication;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { StoreApplication.class })
-public class CucumberSpingConfiguration {}
